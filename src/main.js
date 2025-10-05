288fcd7c3f6675e99cb6de23d58842b58207d173
AgentVen <agentven0x@gmail.com>
2025-10-05 07:12:21 -0600

    working on topbar/menubar

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
 console.log("Started")
+
+
+window.addEventListener('DOMContentLoaded', () => {
+	const menuItems = document.querySelectorAll('.menu-list-item');
+
+	menuItems.forEach((element) => {
+		const button = element.querySelector('.menu-button');
+
+		button.addEventListener('click', () => {
+			if (!button.classList.contains('selected')) {
+				button.classList.add('selected');
+			} else {
+				button.classList.remove('selected');
+			}
+		});
+	});
+	
+});
