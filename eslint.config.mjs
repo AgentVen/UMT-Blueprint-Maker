b0bab5d5b10bea6ccc74e91700b4dffe00777a34
AgentVen <agentven0x@gmail.com>
2025-10-11 10:58:57 -0600

    slight adjustment

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,10 +11,11 @@ export default [
 		rules: {
 			'@stylistic/semi': ['error', 'always'],
 			'@stylistic/brace-style': ['error', '1tbs'],
+			'@stylistic/spaced-comments': ['off'],
 			// [NOTE] Remember to disable these and fix the code later
-			'@stylistic/no-tabs': ['off', 'always'],
-			'@stylistic/indent': ['off', 'always'],
-			'@stylistic/quotes': ['off', 'alawys'],
+			'@stylistic/no-tabs': ['off'],
+			'@stylistic/indent': ['off'],
+			'@stylistic/quotes': ['off'],
 		},
 	},
 ];
