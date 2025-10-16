c743a6d6fdefdd3979752ed7b19dd7c671975837
AgentVen <agentven0x@gmail.com>
2025-10-16 08:17:04 -0600

    Some cleanup

--- src/main.js
+++ src/main.js
@@ -8,53 +8,22 @@ const footer = document.querySelector('footer');
 const elTopbar = header.querySelector('#topbar');
 const elMenuSelector = elTopbar.querySelector('#menu-selector');
 const menuSelections = elMenuSelector.querySelectorAll('.menu-selection');
-//const elMenuPanelContainer = header.querySelector('#menu-panel-container');
 const menuPanels = elMenuSelector.querySelectorAll('.menu-panel');
-
 
 
 // Adjust scales and positions of menu panels
 {
-	// Adjust the min-width of the #menu-panel-container to that of the #menu-selector's width.
-	//elMenuPanelContainer.style.minWidth = `${elMenuSelector.scrollWidth}px`;
-
-	// Adjust the left position of the #menu-panel-container so that its left edge lines up with #menu-selector's left edge
-	/*{
-		const elTopbarInnoLogo = elTopbar.querySelector('#innovation-inc-logo');
-		const topbarInnoLogoComputedStyleMap = elTopbarInnoLogo.computedStyleMap();
-		const marginLeft = topbarInnoLogoComputedStyleMap.get('margin-left');
-		const marginRight = topbarInnoLogoComputedStyleMap.get('margin-right');
-		elMenuPanelContainer.style.left = `calc(${elTopbarInnoLogo.scrollWidth}px + ${marginLeft.value}${marginLeft.unit} + ${marginRight.value}${marginRight.unit})`;
-	}*/
-
-	
-	//let leftOffset = 0
 	menuPanels.forEach((menuPanel, i) => {
 
-		// Adjust the min-widths of each .menu-panel to match the width of their 
-		// corresponding .menu-selection.
-		// 
-		// Then add the width to an offset that will be used to apply a margin-left
-		// to subsequent .menu-panels, so that their left edge is aligned with their
-		// corresponding .menu-selection's left edge.
-		// (this is not done for #menu-panel-0 as #menu-panel-container should be 
-		// aligned with left edge of the #menu-selector)
-
+		// Adjust the min-widths of each .menu-panel to match the width of their corresponding .menu-selection
 		const width = menuSelections.item(i).scrollWidth;
 		menuPanel.style.minWidth = `${width}px`;
-		
-		/*if (leftOffset > 0) {
-			menuPanel.style.marginLeft = `${leftOffset}px`
-		}
-
-		leftOffset += width*/
 
 
 		// Adjust children padding
 		// 
 		// The first child should have its top padding set to 0, and the last child 
 		// should have its bottom padding set to 0.
-
 		const firstItem = menuPanel.firstElementChild;
 		const lastItem = menuPanel.lastElementChild;
 
@@ -67,9 +36,6 @@ const menuPanels = elMenuSelector.querySelectorAll('.menu-panel');
 			}
 
 			lastItem.style.paddingBottom = '0';
-			/*if (lastItem.classList.contains('menu-panel-section')) {
-				lastItem.lastElementChild.style.paddingBottom = '0';
-			}*/
 		}
 
 		// If last child is a .menu-panel-section then set bottom border to 0
@@ -79,7 +45,17 @@ const menuPanels = elMenuSelector.querySelectorAll('.menu-panel');
 	});
 }
 
-
+/**
+ * Sets the selected menu.
+ * 
+ * 0 repersents the first menu, while -1 repersent no menu.
+ * 
+ * No more than one menu can be selected at a time, selecting a menu will automaticly 
+ * deselect any selected menu. Though -1 does not repersent a menu, it still repersents 
+ * a selection, and thus will deselect any selected menus.
+ * @param {number} index A integer from -1 to total number of menus minus 1.
+ * @returns {boolean} Wheather or not a menu was actually selected.
+*/
 function setSelectedMenu(index) {
 	let wasAMenuSelected = false;
 
@@ -101,7 +77,7 @@ function setSelectedMenu(index) {
 	return wasAMenuSelected;
 }
 
-function getSelectedMenu() {
+/*function getSelectedMenu() {
 	menuList.forEach((menuListItem) => {
 		if (menuListItem.classList.contains('selected')) {
 			return menuListItem, i;
@@ -109,20 +85,20 @@ function getSelectedMenu() {
 	});
 
 	return null, -1;
-}
+}*/
 
 // Menu selection logic
 {
 	let wasAMenuElementClicked = false;
 
-	// Setup event listeners for each menu-list-item
+	// First, setup a "click" event listener for each .menu-selection
 	menuSelections.forEach((menuSelection, i) => {
 		menuSelection.addEventListener('click', () => {
 			wasAMenuElementClicked = setSelectedMenu(i);
 		});
 	});
 
-	// Setup event listeners for each button in the menu list panel.
+	// Second, setup "click" event listeners for each .menu-panel-item
 	menuPanels.forEach((menuPanel, i) => {
 		const menuPanelItems = menuPanel.querySelectorAll('.menu-panel-item');
 
@@ -135,11 +111,11 @@ function getSelectedMenu() {
 		});
 	});
 
-	// Event listener for entire document
+	// Last, Event listener for entire document
 	// 
 	// If no kind of menu element was intracted with on this click,
 	// then deselect any selected menu. Otherwise, reset 
-	// wasAMenuElementBeenClicked back to false for the next click.
+	// wasAMenuElementClicked back to false for the next click.
 	document.addEventListener('click', () => {
 		if (!wasAMenuElementClicked) {
 			setSelectedMenu(-1);
