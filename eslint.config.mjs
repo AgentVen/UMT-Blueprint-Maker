cd51587e89ca46264fb14e015a05c5e99bfa699c
AgentVen <agentven0x@gmail.com>
2025-12-16 12:52:56 -0700

    installed eslint packages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,10 +12,9 @@ export default [
 			'@stylistic/semi': ['error', 'always'],
 			'@stylistic/brace-style': ['error', '1tbs'],
 			'@stylistic/spaced-comments': ['off'],
-			// [NOTE] Remember to disable these and fix the code later
 			'@stylistic/no-tabs': ['off'],
 			'@stylistic/indent': ['off'],
-			'@stylistic/quotes': ['off'],
+			'@stylistic/no-multiple-empty-lines': ['off'],
 		},
 	},
 ];
